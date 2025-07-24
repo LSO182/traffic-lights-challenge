2e3ef43e0aeb31f26c4c2b8e5dcac8a503e5a445
sanchezolmedoluciano@gmail.com <sanchezolmedoluciano@gmail.com>
2025-07-24 01:04:26 -0300

    feat: mejora manejo de luces

--- src/main.js
+++ src/main.js
@@ -14,23 +14,33 @@ document.addEventListener("DOMContentLoaded", () => {
 
   for (let i = 0; i < timeUnit; i++) {
     if (i === 5) {
-      road[4] = road[4] === "G" ? "R" : "G";
-      road[8] = road[8] === "R" ? "G" : "R";
+      if (road[4] === "G") road[4] = "O";
+      if (road[8] === "R") road[8] = "O";
     }
 
     if (i === 6) {
-      road[4] = road[4] === "R" ? "G" : "R";
-      road[8] = road[8] === "G" ? "R" : "G";
+      if (road[4] === "O") road[4] = "R";
+      if (road[8] === "O") road[8] = "G";
     }
 
-    if (i === 10) {
-      road[4] = road[4] === "G" ? "R" : "G";
-      road[8] = road[8] === "R" ? "G" : "R";
+    if (i === 11) {
+      if (road[4] === "R") road[4] = "O";
+      if (road[8] === "G") road[8] = "O";
     }
 
-    if (i === 15) {
-      road[4] = road[4] === "R" ? "G" : "R";
-      road[8] = road[8] === "G" ? "R" : "G";
+    if (i === 12) {
+      if (road[4] === "O") road[4] = "G";
+      if (road[8] === "O") road[8] = "R";
+    }
+
+    if (i === 17) {
+      if (road[4] === "G") road[4] = "O";
+      if (road[8] === "R") road[8] = "O";
+    }
+
+    if (i === 18) {
+      if (road[4] === "O") road[4] = "R";
+      if (road[8] === "O") road[8] = "G";
     }
 
     const nextPos = position + i;
@@ -74,6 +84,6 @@ document.addEventListener("DOMContentLoaded", () => {
       }
     }
 
-    console.log(`Ruta ${i + 1}:`, road);
+    console.log(road);
   }
 });
