18047086142803a3fe32a26d380ed4a32fb0ae53
sanchezolmedoluciano@gmail.com <sanchezolmedoluciano@gmail.com>
2025-07-23 21:28:35 -0300

    feat: manejo de semaforos y cambio de luces

--- src/main.js
+++ src/main.js
@@ -1,31 +1,57 @@
 document.addEventListener("DOMContentLoaded", () => {
   const timeUnit = 16;
   let road = ["C", "C", "C", ".", "G", ".", ".", ".", "R", ".", ".", "."];
-  let position = 2;
+  let position = 3;
   let stepsMoved = 0;
   let stopTheCar = false;
 
   for (let i = 0; i < timeUnit; i++) {
+    if (i === 5) {
+      road[4] = road[4] === "G" ? "R" : "G";
+      road[8] = road[8] === "R" ? "G" : "R";
+    }
+
+    if (i === 6) {
+      road[4] = road[4] === "R" ? "G" : "R";
+      road[8] = road[8] === "G" ? "R" : "G";
+    }
+
+    if (i === 10) {
+      road[4] = road[4] === "G" ? "R" : "G";
+      road[8] = road[8] === "R" ? "G" : "R";
+    }
+
+    if (i === 15) {
+      road[4] = road[4] === "R" ? "G" : "R";
+      road[8] = road[8] === "G" ? "R" : "G";
+    }
+
     if (i < 5 && stepsMoved < 4 && !stopTheCar) {
-      if (road[position + i] === "." || road[position + i] === "G") {
+      if (
+        road[position + i] === "." ||
+        road[position + i] === "G" ||
+        road[position - 1] === "R"
+      ) {
         road[position + i] = "C";
         road[position - 1] = ".";
       } else if (road[position + i] === "R") {
+        road[position - 1];
         stopTheCar = true;
       }
     }
 
-    if (i > 6 && stepsMoved < 9 && !stopTheCar) {
-      console.log("asdasd", i);
-    } else if (road[position + i] === "R") {
-      stopTheCar = true;
-    }
+    if (i > 6 && stepsMoved < 4 && !stopTheCar) if (
+        road[position + i] === "." ||
+        road[position + i] === "G" ||
+        road[position - 1] === "R"
+      ) {
+        road[position + i] = "C";
+        road[position - 1] = ".";
+      } else if (road[position + i] === "R") {
+        road[position - 1];
+        stopTheCar = true;
+      }
 
     console.log(`Iteración ${i + 1}:`, road);
-
-    if (i === 4) {
-      console.log("Estamos en la quinta iteración");
-      console.log("Elemento en la posición 4 de road:", road[4], road[8]);
-    }
   }
 });
