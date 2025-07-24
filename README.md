f59dbc60c472bd1f94a7f06b88b18b9af07c262d
sanchezolmedoluciano@gmail.com <sanchezolmedoluciano@gmail.com>
2025-07-24 13:34:15 -0300

    fix: semaforos que se perdian cuando pasaba una c por encima, feat readme

---
+++ README.md
@@ -0,0 +1,24 @@
+# Mini BEM Challenge
+
+## Descripción
+
+Desafio traffic lights de codewars "https://www.codewars.com/kata/5d230e119dd9860028167fa5"
+
+## Tecnologías utilizadas
+
+- JavaScript
+
+## Instalación y ejecución
+
+1. Clonar el repositorio:
+   git clone https://github.com/LSO182/traffic-lights-challenge
+   cd tu-repositorio
+   npm install
+   npm run dev
+## Requisitos
+- Los resultados del desafio se encuentran en la consola del navegador (presionar f12 o click derecho en pantalla y seleccionar inspeccionar)
+- Node.js v20 (se recomienda usar nvm para gestionar versiones)
+
+Para cambiar a la versión correcta:
+
+nvm use 20
--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
   const timeUnit = 16;
+  // calle
   let road = ["C", "C", "C", ".", "G", ".", ".", ".", "R", ".", ".", "."];
+  // calle limpia
+  let underCar = [".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."];
 
+  // for para la unidad de tiempo
   for (let i = 0; i < timeUnit; i++) {
+    // manejo de semáforos
     if (i === 5) {
       if (road[4] === "G") road[4] = "O";
       if (road[8] === "R") road[8] = "O";
@@ -27,30 +32,38 @@ document.addEventListener("DOMContentLoaded", () => {
       if (road[4] === "O") road[4] = "R";
       if (road[8] === "O") road[8] = "G";
     }
+    // copio los caminos para actualizar valores y evitar conflicto
+    const newRoad = [...road];
+    const newUnderCar = [...underCar];
 
-    let newRoad = [...road];
-
+    // for para mover los autos
     road.forEach((street, index) => {
+      // capturo el index de cada auto y le asigno el valor a su proxima posicion
       if (street === "C") {
         const currentPosition = index;
         const nextPosition = currentPosition + 1;
+        // mover auto
 
         if (nextPosition < road.length) {
           const nextStreet = road[nextPosition];
 
           if (nextStreet === "." || nextStreet === "G") {
+            // guardo lo que esta debajo del auto
+            newUnderCar[nextPosition] = road[nextPosition];
             newRoad[nextPosition] = "C";
-            newRoad[currentPosition] = ".";
-            console.log(`Moviendo C de ${currentPosition} a ${nextPosition}`);
-          } else {
-            console.log(`C en ${currentPosition} no puede avanzar. Siguiente: ${nextStreet}`);
+
+            // devuelvo el valor que habia antes que pasara por encima el auto
+            newRoad[currentPosition] = underCar[currentPosition];
+            newUnderCar[currentPosition] = ".";
           }
         }
       }
     });
 
+    // actualizo el estado
     road = newRoad;
+    underCar = newUnderCar;
 
-    console.log(road)
+    console.log(road);
   }
 });
