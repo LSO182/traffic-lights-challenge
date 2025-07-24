3828bf05caaae732d5333a0832d91dacd3e34e81
sanchezolmedoluciano@gmail.com <sanchezolmedoluciano@gmail.com>
2025-07-24 01:44:14 -0300

    feat: car current position

--- src/main.js
+++ src/main.js
@@ -6,38 +6,33 @@ document.addEventListener("DOMContentLoaded", () => {
   let stopTheCar = false;
   let cars = [];
 
-  for (let i = 0; i < road.length; i++) {
+   for (let i = 0; i < road.length; i++) {
     if (road[i] === "C") {
-      cars.push(i);
+      cars.push({ initial: i, current: i });
     }
   }
 
   for (let i = 0; i < timeUnit; i++) {
-    if (i === 5) {
+   if (i === 5) {
       if (road[4] === "G") road[4] = "O";
       if (road[8] === "R") road[8] = "O";
     }
-
     if (i === 6) {
       if (road[4] === "O") road[4] = "R";
       if (road[8] === "O") road[8] = "G";
     }
-
     if (i === 11) {
       if (road[4] === "R") road[4] = "O";
       if (road[8] === "G") road[8] = "O";
     }
-
     if (i === 12) {
       if (road[4] === "O") road[4] = "G";
       if (road[8] === "O") road[8] = "R";
     }
-
     if (i === 17) {
       if (road[4] === "G") road[4] = "O";
       if (road[8] === "R") road[8] = "O";
     }
-
     if (i === 18) {
       if (road[4] === "O") road[4] = "R";
       if (road[8] === "O") road[8] = "G";
@@ -45,33 +40,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const nextPos = position + i;
 
-    if (i <= 5 && stepsMoved < 5 && !stopTheCar) {
-      if (
-        (road[nextPos] === "." || road[nextPos] === "G") &&
-        road[nextPos] !== "C"
-      ) {
-        road[nextPos] = "C";
-        road[position - 1] = ".";
-        stepsMoved++;
-      } else if (road[nextPos] === "R") {
-        stopTheCar = true;
-      }
-    }
-
-    if (i > 6 && stepsMoved < 5 && !stopTheCar) {
-      if (
-        (road[nextPos] === "." || road[nextPos] === "G") &&
-        road[nextPos] !== "C"
-      ) {
-        road[nextPos] = "C";
-        road[position - 1] = ".";
-        stepsMoved++;
-      } else if (road[nextPos] === "R") {
-        stopTheCar = true;
-      }
-    }
-
-    if (i > 10 && stepsMoved < 5 && !stopTheCar) {
+    if (stepsMoved < 5 && !stopTheCar) {
       if (
         (road[nextPos] === "." || road[nextPos] === "G") &&
         road[nextPos] !== "C"
